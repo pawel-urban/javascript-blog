94258370980fa87b5f336431c477710be706546a
Paweł Urban <pawel.urban.pmg@gmail.com>
2021-04-28 15:06:36 +0200

    Add 'get 'href'' to function js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,9 @@ document.getElementById('test-button').addEventListener('click', function(){
 });
 */
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
+
   console.log('clickedElement (with plus): ' + clickedElement);
 
   /* [DONE] remove class 'active' from all article links  */
@@ -16,8 +18,7 @@ const titleClickHandler = function(event){
   for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
   }
-  /* [IN PROGRESS] add class 'active' to the clicked link */
-
+  /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
@@ -25,9 +26,10 @@ const titleClickHandler = function(event){
   for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
   }
-  /* get 'href' attribute from the clicked link */
-
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href')
+  console.log('href:', articleSelector)
+  /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
 }
