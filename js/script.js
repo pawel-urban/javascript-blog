a86914706152eb0302ff8f939597c18ebfede0cb
Paweł Urban <pawel.urban.pmg@gmail.com>
2021-04-28 18:56:42 +0200

    Remove consol.logs from code

--- js/script.js
+++ js/script.js
@@ -3,14 +3,11 @@
 /*
 document.getElementById('test-button').addEventListener('click', function(){
   const links = document.querySelectorAll('.titles a');
-  console.log('links:', links);
 });
 */
 const titleClickHandler = function(event){
   event.preventDefault();
   const clickedElement = this;
-
-  console.log('clickedElement (with plus): ' + clickedElement);
 
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -29,11 +26,9 @@ const titleClickHandler = function(event){
   /* [DONE] get 'href' attribute from the clicked link */
   const articleSelector = clickedElement.getAttribute('href');
 
-  console.log('href:', articleSelector);
   /* [DONE] find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
 
-  console.log(targetArticle);
   /* [DONE] add class 'active' to the correct article */
   targetArticle.classList.add('active');
 }
